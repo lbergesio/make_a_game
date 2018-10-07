cae22f29fdc4c2576d7628c02d96d479eccd2c3b
Leonardo Bergesio <leo@voltanet.io>
2018-10-07 23:20:57 +0200

    End of video 3

---
+++ src/2d_engine/game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_HEADER
+#define GAME_HEADER
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+class Game {
+
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height,
+							bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running(){return isRunning;};
+
+private:
+	int cnt = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+
+#endif //GAME_HEADER
